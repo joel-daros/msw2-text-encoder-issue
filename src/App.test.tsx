1c3d6f8cf1811cf0fecc4f403829869e0c85daa2
Joel Daros <joel.daros@corteva.com>
2023-10-27 10:08:29 -0300

    added msw

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { setupServer } from "msw/node";
+import { HttpResponse, http } from "msw";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("should pass, but doesn't", () => {
+  server.use(
+    http.get("/test", () => {
+      return HttpResponse.json(null, {
+        status: 500,
+      });
+    })
+  );
 });
