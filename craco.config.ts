f5ecdb55545c3eb23be03c229f99ca24febcaaf9
Joel Daros <joel.daros@gmail.com>
2023-11-20 10:08:30 -0300

    updated craco.config.ts

--- craco.config.ts
+++ craco.config.ts
@@ -3,10 +3,8 @@ import { CracoConfig } from "@craco/types";
 const cracoConfig: CracoConfig = {
   jest: {
     configure: (jestConfig, { env, paths, resolve, rootDir }) => {
-      return {
-        ...jestConfig,
-        setupFiles: [`${rootDir}/src/jest.polyfills`],
-      };
+      jestConfig.setupFiles = [`${rootDir}/src/jest.polyfills.js`];
+      return jestConfig;
     },
   },
 };
